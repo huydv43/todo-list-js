f489973e2b168539a5fe2497e177539c72cfb382
huydv43 <huydv43@gmail.com>
2021-03-30 23:35:30 +0700

    todo list app implementation

--- script.js
+++ script.js
@@ -1,77 +1,82 @@
 const todoList = [];
 const todoListElement = document.querySelector('#myUL')
-document.querySelector('#add_button').addEventListener("click", addTodo);
+const day = new Date();
+const currentDate = [day.getDate(), day.getMonth() + 1, day.getFullYear()].join("/");
+document.querySelector("#add_button").addEventListener("click", addTodo);
 
 function addTodo(event){
     const title = document.querySelector('#title').value;
     const content = document.querySelector('#content').value;
-    // console.log(title);
-    // console.log(content);
     
 
-    if(title === '' || content === ''){
-        alert('nhap todo');
-    }
-    else{
-        const Todo = {
-          id: todoList.length,
-          title: title,
-          content: content,
-          date: new Date(),
-        };
-        todoList.push(Todo);
-        displayTodos();
-    }
+    if (title === '' || content === '') {
+        const errorMessage = "please enter valid form";
+        const elementMessage = document.querySelector("#error");
+        elementMessage.innerHTML = errorMessage;
+      } else {
+          const Todo = {
+            id: todoList.length,
+            title: title,
+            content: content,
+            day: currentDate,
+          };
+          todoList.push(Todo);
+          displayTodos();
+        }
     event.preventDefault();
 }
+function clearMessage() {
+  const elementError = document.querySelector("#error");
+  elementError.innerHTML = "";
+}
+
 function deleteItem(x) {
   todoList.splice(
     todoList.findIndex((item) => item.id == x),
     1
   );
   displayTodos();
-  
 }
-
-
 
 function displayTodos(){
     todoListElement.innerHTML = '';
     document.querySelector("#title").value = '';
     document.querySelector("#content").value = '';
     todoList.forEach((item) =>{
+        const idTodo = document.createElement("span");
         const listElement = document.createElement("div");
-        const span = document.createElement('span');
-        const spanDelete = document.createElement('span');
-        const h2 = document.createElement('h2');
-        const p = document.createElement('p');
-        const currentDate = item.date;
-        const month = currentDate.getUTCMonth() + 1;
-        const day = currentDate.getUTCDate();
-        const year = currentDate.getUTCFullYear();
-        newdate = year + "/" + month + "/" + day;
+        const timeTodo = document.createElement('span');
+        const deleteTodo = document.createElement('span');
+        const titleTodo = document.createElement('h2');
+        const contentTodo = document.createElement('p');
+        
         listElement.setAttribute("class","list-element");
-        h2.setAttribute("class","title");
-        spanDelete.setAttribute("class","btn-delete");
-        span.setAttribute("class","time");
+        titleTodo.setAttribute("class", "title");
+        contentTodo.setAttribute("class","main-content");
+        deleteTodo.setAttribute("class","btn-delete");
+        timeTodo.setAttribute("class","time");
+        idTodo.setAttribute("class","id-todo");
         
+        idTodo.innerHTML = item.id + 1;
+        timeTodo.innerHTML = currentDate;
+        titleTodo.innerHTML = item.title;
+        contentTodo.innerHTML = item.content;
+        deleteTodo.innerHTML = "X";
         
-        span.innerHTML = newdate;
-        h2.innerHTML = item.title;
-        p.innerHTML = item.content;
-        spanDelete.innerHTML = "X";
-        // listElement.appendChild(spanDelete);
-        listElement.appendChild(h2);
-        listElement.appendChild(span);
-        listElement.appendChild(p);
+        listElement.appendChild(idTodo);
+        listElement.appendChild(titleTodo);
+        listElement.appendChild(timeTodo);
+        listElement.appendChild(deleteTodo);
+        listElement.appendChild(contentTodo);
+        
         todoListElement.appendChild(listElement);
-        todoListElement.appendChild(spanDelete);
         
-         spanDelete.setAttribute("data-id", item.id);
-         spanDelete.addEventListener("click", function (e) {
+        
+         deleteTodo.setAttribute("data-id", item.id);
+         deleteTodo.addEventListener("click", function (e) {
            const idItem = e.target.getAttribute("data-id");
            deleteItem(idItem);
          });
     });
-    
+    clearMessage();
 }
